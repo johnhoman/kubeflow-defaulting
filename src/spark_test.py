4c0a3741e0352fe1e962398fa6d28202419e8355
Jack Homan <homan.jack@gmail.com>
2022-02-28 00:59:07 -0500

    spark driver defaults idempotent

--- src/spark_test.py
+++ src/spark_test.py
@@ -1,4 +1,3 @@
-import json
 import base64 as b64
 
 from jsonpatch import JsonPatch
@@ -6,13 +5,34 @@ from jsonpatch import JsonPatch
 from src import admission_review
 
 
+def decode_patch(review: admission_review.AdmissionReview) -> dict:
+    patch = JsonPatch.from_string(b64.b64decode(review.response.patch).decode())
+    obj = patch.apply(review.request.object)
+    return obj
+
+
 def test_spark_pod_has_block_manager_port_defined(client, pod_admission_review):
     obj = client.post(
         "/spark/mutate-driver-core-v1-pod", json=pod_admission_review.dict()
     ).json()
     res = admission_review.AdmissionReview(**obj)
-    patch = JsonPatch.from_string(b64.b64decode(res.response.patch).decode())
-    pod = patch.apply(pod_admission_review.request.object)
+    pod = decode_patch(res)
     ports = {port["name"]: port for port in pod["spec"]["containers"][0]["ports"]}
     assert "blockmanager" in ports
     assert ports["blockmanager"]["containerPort"] == 7777
+
+
+def test_spark_default_is_idempotent(client, pod_admission_review):
+
+    obj = client.post(
+        "/spark/mutate-driver-core-v1-pod", json=pod_admission_review.dict()
+    ).json()
+    res = admission_review.AdmissionReview(**obj)
+    before = decode_patch(res)
+    obj = client.post(
+        "/spark/mutate-driver-core-v1-pod",
+        json=admission_review.AdmissionReview(**obj).dict(),
+    ).json()
+    res = admission_review.AdmissionReview(**obj)
+    after = decode_patch(res)
+    assert before == after
