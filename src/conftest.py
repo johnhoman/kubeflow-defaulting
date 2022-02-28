0ad1731c24d608d5741232ebb9285314cae44976
Jack Homan <homan.jack@gmail.com>
2022-02-28 00:35:18 -0500

    move fixtures to conftest

--- src/conftest.py
+++ src/conftest.py
@@ -1,7 +1,7 @@
 from fastapi.testclient import TestClient
 import pytest
 
-from src import main
+from src import main, admission_review
 
 
 @pytest.fixture(scope="function")
@@ -14,3 +14,50 @@ def app():
 @pytest.fixture(scope="function")
 def client(app):
     return TestClient(app)
+
+
+@pytest.fixture(scope="function")
+def pod_admission_review():
+    ar = {
+        "kind": "AdmissionReview",
+        "apiVersion": "admission.k8s.io/v1beta1",
+        "request": {
+            "uid": "0df28fbd-5f5f-11e8-bc74-36e6bb280816",
+            "kind": {"group": "", "version": "v1", "kind": "Pod"},
+            "resource": {"group": "", "version": "v1", "resource": "pods"},
+            "namespace": "dummy",
+            "operation": "CREATE",
+            "userInfo": {
+                "username": "system:serviceaccount:kube-system:replicaset-controller",
+                "uid": "a7e0ab33-5f29-11e8-8a3c-36e6bb280816",
+                "groups": [
+                    "system:serviceaccounts",
+                    "system:serviceaccounts:kube-system",
+                    "system:authenticated",
+                ],
+            },
+            "object": {
+                "metadata": {
+                    "name": "test-pod",
+                    "namespace": "testspace-1234",
+                },
+                "spec": {
+                    "volumes": [],
+                    "containers": [
+                        {
+                            "name": "main",
+                            "image": "python:3.8",
+                        }
+                    ],
+                    "restartPolicy": "Always",
+                    "serviceAccountName": "default",
+                    "serviceAccount": "default",
+                },
+                "status": {},
+            },
+            "oldObject": None,
+        },
+    }
+    return admission_review.AdmissionReview(**ar)
+
+
