026e96d9fe37c120f4246436f3ea44026abc1288
Jack Homan <homan.jack@gmail.com>
2022-02-27 23:38:06 -0500

    rename

--- src/main.py
+++ src/main.py
@@ -4,10 +4,10 @@ import fastapi
 from src import spark
 
 
-api = fastapi.FastAPI()
-api.include_router(spark.router, prefix="/spark")
+app = fastapi.FastAPI()
+app.include_router(spark.router, prefix="/spark")
 
 
-@api.get("/healthz")
+@app.get("/healthz")
 def health_check():
     pass
