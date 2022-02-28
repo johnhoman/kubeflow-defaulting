f8b7616b16b047719c772102e21284bc1a2338de
Jack Homan <homan.jack@gmail.com>
2022-02-27 22:17:29 -0500

    starting spark defaults

--- src/depends.py
+++ src/depends.py
@@ -1,9 +1,17 @@
 import json
 
-from kubernetes.client import V1Pod
+from fastapi import Depends
+from kubernetes.client import V1Pod, V1PodSpec
 from kubernetes.client import ApiClient
 
 from src.admission_review import AdmissionReview
+
+
+def _ser(cls):
+    def to_dict(self):
+        return ApiClient().sanitize_for_serialization(self)
+    cls.to_dict = to_dict
+    return cls
 
 
 class _Req(object):
@@ -15,9 +23,9 @@ def _deserialize(obj, type_):
     return ApiClient().deserialize(_Req(obj), type_)
 
 
-def _serialize(obj):
-    return ApiClient().sanitize_for_serialization(obj)
+def v1_pod(admission_review: AdmissionReview) -> V1Pod:
+    return _deserialize(admission_review.request.object, _ser(V1Pod))
 
 
-def pod(admission_review: AdmissionReview) -> V1Pod:
-    return _deserialize(admission_review.request.object, V1Pod)
+def v1_pod_spec(pod: V1Pod = Depends(v1_pod)) -> V1PodSpec:
+    return pod.spec
--- src/spark.py
+++ src/spark.py
@@ -1,11 +1,84 @@
 from fastapi import APIRouter, Depends
-from kubernetes.client import V1Pod
+from kubernetes.client import (
+    V1ContainerPort,
+    V1EnvVar,
+    V1EnvVarSource,
+    V1ObjectFieldSelector,
+    V1Pod,
+    V1PodSpec,
+)
 
 from src import depends
+from src.admission_review import AdmissionReview
+
 
 router = APIRouter(tags=["spark"])
 
 
 @router.post("/mutate-driver-core-v1-pod")
-def spark_driver_defaults(pod: V1Pod = Depends(depends.pod)):
-    pass
+def spark_driver_defaults(
+    admission_review: AdmissionReview,
+    pod: V1Pod = Depends(depends.v1_pod),
+    spec: V1PodSpec = Depends(depends.v1_pod_spec),
+):
+    container = spec.containers[0]
+    if container.ports is None:
+        container.ports = []
+
+    ports = set(port.name for port in container.ports)
+    if "driver" not in ports:
+        port = V1ContainerPort(
+            container_port=2222,
+            name="driver",
+            protocol="TCP",
+        )
+        container.ports.append(port)
+
+    if "blockmanager" not in ports:
+        port = V1ContainerPort(
+            container_port=7777,
+            name="blockmanager",
+            protocol="TCP",
+        )
+        container.ports.append(port)
+
+    if container.env is None:
+        container.env = []
+
+    envs = set(env.name for env in container.env)
+    if "POD_NAMESPACE" not in envs:
+        var = V1EnvVar(
+            name="POD_NAMESPACE",
+            value_from=V1EnvVarSource(
+                field_ref=V1ObjectFieldSelector(
+                    field_path="metadata.namespace",
+                )
+            )
+        )
+        container.env.append(var)
+
+    if "POD_NAME" not in envs:
+        var = V1EnvVar(
+            name="POD_NAME",
+            value_from=V1EnvVarSource(
+                field_ref=V1ObjectFieldSelector(
+                    field_path="metadata.name",
+                )
+            )
+        )
+        container.env.append(var)
+
+    if "POD_IP_ADDRESS" not in envs:
+        var = V1EnvVar(
+            name="POD_IP_ADDRESS",
+            value_from=V1EnvVarSource(
+                field_ref=V1ObjectFieldSelector(
+                    field_path="status.podIP",
+                )
+            )
+        )
+        container.env.append(var)
+
+    pod.spec = spec
+    return admission_review.patch(pod.to_dict())
+
