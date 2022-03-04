6a46021a394f3bc3135611ffac80344e64dff04e
Jack Homan <homan.jack@gmail.com>
2022-03-03 21:39:07 -0500

    missing test description

--- src/spark_test.py
+++ src/spark_test.py
@@ -6,12 +6,20 @@ from src import admission_review
 
 
 def decode_patch(review: admission_review.AdmissionReview) -> dict:
+    """
+    decode the patch from the admission response
+    and return the patched object
+    """
+
     patch = JsonPatch.from_string(b64.b64decode(review.response.patch).decode())
     obj = patch.apply(review.request.object)
     return obj
 
 
 def test_spark_pod_has_block_manager_port_defined(client, pod_admission_review):
+    """
+    the webhook exposes port 7777 on the target pod
+    """
     obj = client.post(
         "/spark/mutate-driver-core-v1-pod", json=pod_admission_review.dict()
     ).json()
@@ -23,7 +31,9 @@ def test_spark_pod_has_block_manager_port_defined(client, pod_admission_review):
 
 
 def test_spark_default_is_idempotent(client, pod_admission_review):
-
+    """
+    repeated calls to the webhook produces the same result
+    """
     obj = client.post(
         "/spark/mutate-driver-core-v1-pod", json=pod_admission_review.dict()
     ).json()
