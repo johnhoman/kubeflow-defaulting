253a98ad238c3ddf57df296dac7d297b80bdefd6
Jack Homan <homan.jack@gmail.com>
2022-03-03 21:47:09 -0500

    adding container depends

--- src/depends.py
+++ src/depends.py
@@ -1,7 +1,8 @@
 import json
+import typing
 
 from fastapi import Depends
-from kubernetes.client import V1Pod, V1PodSpec
+from kubernetes.client import V1Pod, V1PodSpec, V1Container
 from kubernetes.client import ApiClient
 
 from src.admission_review import AdmissionReview
@@ -25,8 +26,35 @@ def _deserialize(obj, type_):
 
 
 def v1_pod(admission_review: AdmissionReview) -> V1Pod:
+    """
+    Get the pod object from the AdmissionReview and return
+    the model representation of the object
+
+    Returns
+    -------
+    kubernetes.client.V1Pod
+    """
     return _deserialize(admission_review.request.object, _ser(V1Pod))
 
 
 def v1_pod_spec(pod: V1Pod = Depends(v1_pod)) -> V1PodSpec:
+    """
+    Get the pod spec from the pod object in the AdmissionReview and return
+    the model representation of the object
+
+    Returns
+    -------
+    kubernetes.client.V1PodSpec
+    """
     return pod.spec
+
+
+def v1_container(
+    *,
+    position: int = None,
+    name: str = None,
+) -> typing.Callable[[AdmissionReview], V1Container]:
+    def container(spec: V1PodSpec = Depends(v1_pod_spec)) -> V1Container:
+        return spec.containers[0]
+
+    return container
--- src/depends_test.py
+++ src/depends_test.py
@@ -11,3 +11,10 @@ def test_v1_pod_spec(pod_admission_review):
     pod = depends.v1_pod(pod_admission_review)
     pod_spec = depends.v1_pod_spec(pod)
     assert pod.spec.to_dict() == pod_spec.to_dict()
+
+
+def test_v1_container(pod_admission_review):
+    pod = depends.v1_pod(pod_admission_review)
+    pod_spec = depends.v1_pod_spec(pod)
+    container = depends.v1_container(position=0)(pod_spec)
+    assert pod.spec.containers[0].to_dict() == container.to_dict()
