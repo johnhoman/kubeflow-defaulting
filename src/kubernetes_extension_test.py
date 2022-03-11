8e9fab756cb8b25f3f7eb760d480661988ae9a3f
Jack Homan <homan.jack@gmail.com>
2022-03-10 22:32:13 -0500

    more assertions

--- src/kubernetes_extension_test.py
+++ src/kubernetes_extension_test.py
@@ -36,3 +36,12 @@ def test_can_deserialize_profile():
     assert out.metadata.name == "profile-name"
     assert out.kind == "Profile"
     assert out.api_version == "kubeflow.org/v1"
+    assert out.spec.owner.kind == "User"
+    assert out.spec.owner.name == "homan.jack@gmail.com"
+    assert out.spec.resource_quota_spec.hard == {
+        "cpu": "2",
+        "memory": "2Gi",
+        "requests.nvidia.com/gpu": "1",
+        "persistentvolumeclaims": "1",
+        "requests.storage": "5Gi",
+    }
